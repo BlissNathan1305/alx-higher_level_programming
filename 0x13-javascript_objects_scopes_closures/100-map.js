dca9b681e4b40d4aa17fc8ba2b369cf4574be16b
BlissNathan1305 <48740078+BlissNathan1305@users.noreply.github.com>
2023-03-15 15:17:23 +0100

    JavaScript tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const list = require('./100-data').list;
-const multiIdx = (value, index) => value * index;
-const newlist = list.map(multiIdx);
-console.log(list);
-console.log(newlist);
